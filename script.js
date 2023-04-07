eda740321057adc22455e8b38e7e49681dd277d8
Andrew Novosel <andrewnovosel90@gmail.com>
2023-04-07 10:13:24 -0400

    Changed timeline

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-function positionTimelineEvents() {
-    let events = document.querySelectorAll(".timeline-event");
-    let containerWidth = document.querySelector(".timeline-container").offsetWidth;
-    let eventWidth = events[0].offsetWidth;
-    let totalWidth = eventWidth * events.length + 20 * (events.length - 1);
-  
-    if (totalWidth < containerWidth) {
-        let offset = (containerWidth - totalWidth) / 2;
-      events[0].style.left = offset + "px";
-  
-      for (let i = 1; i < events.length; i++) {
-        offset += eventWidth + 20;
-        events[i].style.left = offset + "px";
-      }
-    } else {
-      for (let i = 0; i < events.length; i++) {
-        events[i].style.left = "";
-      }
-    }
-  }
-  
-  positionTimelineEvents();
-  
-  window.addEventListener("resize", function () {
-    positionTimelineEvents();
-  });
-  
+$(".timeline-event-button").click(function (e) {
+  e.preventDefault();
+  var $container = $(this).closest(".timeline-events-container");
+  var containerWidth = $container.width();
+  var scrollAmount = containerWidth * 0.8;
+  $container.animate({ scrollLeft: "+=" + scrollAmount }, 500);
+});
