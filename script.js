424a1d12859f0dcbed2a0c29cb8362b06e34ae33
Andrew Novosel <andrewnovosel90@gmail.com>
2023-04-21 16:10:56 -0400

    Changed JavaScript code and CSS code

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ const slider = document.querySelector(".timeline-slider");
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 
-const itemWidth =
-  items[0].offsetWidth +
-  2 * parseFloat(getComputedStyle(items[0]).paddingRight);
-const sliderWidth = itemWidth * items.length;
-slider.style.width = sliderWidth + "px";
-
+let itemWidth;
+let sliderWidth;
 let currentIndex = 0;
 
 function setActiveItem(index) {
@@ -21,6 +17,20 @@ function setActiveItem(index) {
 function scrollToItem(index) {
   const position = -index * itemWidth;
   slider.style.transform = `translateX(${position}px)`;
+}
+
+function calculateWidths() {
+  if (window.innerWidth <= 768) {
+    itemWidth = slider.offsetWidth / 2;
+    sliderWidth = itemWidth * items.length;
+  } else {
+    itemWidth =
+      items[0].offsetWidth +
+      2 * parseFloat(getComputedStyle(items[0]).paddingRight);
+    sliderWidth = itemWidth * items.length;
+  }
+
+  slider.style.width = sliderWidth + "px";
 }
 
 function showNextItem() {
@@ -40,6 +50,9 @@ function showPrevItem() {
   setActiveItem(currentIndex);
   scrollToItem(currentIndex);
 }
+
+calculateWidths();
+window.addEventListener("resize", calculateWidths);
 
 nextBtn.addEventListener("click", showNextItem);
 prevBtn.addEventListener("click", showPrevItem);
